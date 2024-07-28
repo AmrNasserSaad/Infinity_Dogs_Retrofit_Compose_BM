1299fcfbff545087e9f27a3a31026ae8d1d9a854
AmrNasser <amr931111@gmail.com>
2024-07-28 10:05:14 +0300

    finish the app

--- app/src/main/java/com/example/infinitydogs/MainActivity.kt
+++ app/src/main/java/com/example/infinitydogs/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.infinitydogs
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.infinitydogs.api.Dog
 import com.example.infinitydogs.api.DogAPiCallable
 import com.example.infinitydogs.ui.theme.InfinityDogsTheme
@@ -18,20 +29,54 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             InfinityDogsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+                    DogImage(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+@Composable
+fun DogImage(modifier: Modifier = Modifier) {
+    var dog by remember { mutableStateOf<Dog?>(null) }
+    var isLoading by remember { mutableStateOf(true) }
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        getDogImage {
+            if (it != null) dog = it else Toast.makeText(
+                context,
+                "Failed to load image",
+                Toast.LENGTH_SHORT
+            ).show()
+            isLoading = false
+        }
+    }
+    if (!isLoading) {
+        AsyncImage(
+            model = ImageRequest
+                .Builder(context)
+                .data(dog?.imageLink)
+                .crossfade(700)
+                .placeholder(R.drawable.ic_downloading)
+                .error(R.drawable.ic_error)
+                .build(),
+            //model = link,
+            //imageLoader = context.imageLoader,
+            contentDescription = "Dog image",
+            modifier = modifier.fillMaxSize()
+        )
+    }
+
+}
 
 fun getDogImage(onReceived: (Dog?) -> Unit) {
     val retrofit = Retrofit
@@ -48,6 +93,7 @@ fun getDogImage(onReceived: (Dog?) -> Unit) {
         }
 
         override fun onFailure(p0: Call<Dog>, p1: Throwable) {
+            Log.d("trace", "Error: ${p1.message}")
             onReceived(null)
         }
     })
